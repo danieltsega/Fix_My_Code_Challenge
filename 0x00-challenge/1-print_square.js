f058bbd5d43a9d71164aeee3a7c09cbfb8ff38db
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-25 13:20:14 +0300

    [Fix]: Fixed the code by adding a NaN condition to check the size argument of the square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,12 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = process.argv[2]
+size = parseInt(process.argv[2])
+
+if (isNaN(size)) {
+    process.stderr.write("Invalid size\n");
+    process.exit(1);
+}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
