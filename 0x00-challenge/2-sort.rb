64e9dd4d96ff845a2d779c3da06e42836a653348
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-27 10:12:54 +0300

    [Fix]: Fixed the entire ruby code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -7,7 +7,7 @@
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next unless arg = ~ /^-?\d+$/
+    next if arg !~ /^-?[0-9]+$/
 
     # convert to integer
     i_arg = arg.to_i
@@ -17,7 +17,7 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] <= i_arg
+        if result[i] < i_arg
             i += 1
         else
             result.insert(i, i_arg)
@@ -25,7 +25,7 @@ ARGV.each do |arg|
             break
         end
     end
-    result << i_arg unless is_inserted
+    result << i_arg if !is_inserted
 end
 
 puts result
